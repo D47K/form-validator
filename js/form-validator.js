937913af9aa8aa5763b1539b663f33e078639d96
D47K <skorpio1197@gmail.com>
2019-09-27 09:56:14 -0500

    1. Datos del API

---
+++ js/form-validator.js
@@ -0,0 +1,10 @@
+/*
+ * Form-Validator
+ * Authors: Andres Hilario Vidal.
+ * All Rights Reserved.
+ * Use, reproduction, distribution, and modification of this code is subject to the terms and
+ * conditions of the MIT license, available at http://www.opensource.org/licenses/mit-license.php
+ *
+ * Project: https://github.com/D47K/form-validator
+ */
+/* global define */
